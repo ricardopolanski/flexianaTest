e045fb6ddfe4b521fd3f40b595fce3fb07d9f3b8
Ricardo Polanski <rpolanski@live.com>
2022-10-11 17:40:32 -0300

    commit pageObj

---
+++ cypress/integration/pageObjects/elements.js
@@ -0,0 +1,23 @@
+function pageElements() {
+    const page = '[class="page"]';
+    const startGame = '[href="./"]';
+    const space02 = '[name="space02"]';
+    const space13 = '[name="space13"]';
+    const space15 = '[name="space15"]';
+    const space22 = '[name="space22"]';
+    const space24 = '[name="space24"]';
+    const space33 = '[name="space33"]';
+
+    return {
+        page,
+        startGame,
+        space02,
+        space13,
+        space15,
+        space22,
+        space24,
+        space33
+    }
+}
+
+export  {pageElements}
